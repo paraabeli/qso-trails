0fd8a07b5d39204bbc1ce665a29ba7de3e947705
paraabeli <paraabeli@users.noreply.github.com>
2026-08-22 23:59:00 +0300

    Add security helper regression tests

---
+++ test/security-helpers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+const { parseCoord, safeEqual } = require('../security-helpers');
+
+assert.strictEqual(safeEqual('admin', 'admin'), true);
+assert.strictEqual(safeEqual('admin', 'Admin'), false);
+assert.strictEqual(safeEqual('short', 'shorter'), false);
+assert.strictEqual(safeEqual('x'.repeat(4097), 'x'.repeat(4097)), false);
+
+assert.strictEqual(parseCoord('60.1699', true), 60.1699);
+assert.strictEqual(parseCoord('N 60 10.194', true), 60 + 10.194 / 60);
+assert.strictEqual(parseCoord('60:10.194N', true), 60 + 10.194 / 60);
+assert.strictEqual(parseCoord('W 024 56.220', false), -(24 + 56.22 / 60));
+assert.strictEqual(parseCoord('N 024 56.220', false), null);
+assert.strictEqual(parseCoord('91', true), null);
+assert.strictEqual(parseCoord('180:60E', false), null);
+assert.strictEqual(parseCoord('N' + ' '.repeat(100000) + '60 10', true), null);
+assert.strictEqual(parseCoord('60 10 N S', true), null);
+
+console.log('security helper tests passed');
